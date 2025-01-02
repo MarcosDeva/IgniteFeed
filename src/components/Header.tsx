68f38dfabc6f4b60b74ba7bf46d87a2e2d3e7af1
Marcos <marcos.oliveira.developer@gmail.com>
2025-01-02 08:35:35 -0300

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export function Header(){
     return (
         <header className={styles.header}>
-            <strong> IgniteFeed</strong>
+            <img src={igniteLogo} alt="Logotido do Ignite" />
+            <strong> Ignite Feed</strong>
         </header>
         
     );
