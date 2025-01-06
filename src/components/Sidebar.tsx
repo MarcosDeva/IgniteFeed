5a033b3b47eefe333de11ca50459dc96f67bba87
Marcos <marcos.oliveira.developer@gmail.com>
2025-01-06 17:54:36 -0300

    Adicionado

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,23 @@
+// import React from 'react';
+// import { ReactDOM } from 'react';
+
 import styles from './Sidebar.module.css';
 export function Sidebar(){
     return (
         <aside className={styles.sidebar}>
-            <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=500" alt="text" />
+            <img className={styles.cover} 
+                 src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=500" alt="text" 
+            />
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/MarcosDeva.png" alt="" />
+                <strong> Marcos Oliveira</strong>
+                <span> Web Developer</span>
+            </div>
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
         </aside>
     );
 }
