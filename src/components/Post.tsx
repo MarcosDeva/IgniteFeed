10462815d8d8eb7edea7273d4243d68eef7e875e
Marcos <marcos.oliveira.developer@gmail.com>
2025-02-01 09:51:47 -0300

    refatorado

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -70,9 +70,9 @@ export function Post({ author, publishedAt, content }){
 
             <div className={styles.content}>
                {content.map(line => {
-                if(line.type == 'paragraph'){
+                if(line.type === 'paragraph'){
                     return <p key={line.content}>{line.content}</p>;
-                }else if(line.type == 'link'){
+                }else if(line.type === 'link'){
                     return <p key={line.content}><a href="#">{line.content}</a></p>
                 }
                })}
